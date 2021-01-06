af80403dc96b014c629538d862623a249572bbb3
Gzim <gzimasanni@gmail.com>
2021-01-06 17:57:57 +0100

    Build user rescource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Gzim", password: "zhunicali")
+User.create(username: "Asani", password: "zhunicali")
+User.create(username: "Mila", password: "zhunicali")
+User.create(username: "Maka", password: "zhunicali")
+User.create(username: "Mina", password: "zhunicali")
